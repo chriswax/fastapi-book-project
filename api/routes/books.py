69d71a26a8c3e27dc88bd8c2b4731c7ea56ae936
chriswax <chriswax.ent@gmail.com>
2025-02-11 18:40:12 +0100

    Feat/Added Get Book End Point Add CI pipeline

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,6 +48,16 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
+@router.get("/{book_id}", status_code=status.HTTP_200_OK, response_model=Book)
+async def get_book_by_id(book_id: int):
+    book = db.books.get(book_id)
+    if not book:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": "Book not found"},
+        )
+    return book
+
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
     return JSONResponse(
